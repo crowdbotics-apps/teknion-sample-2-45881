2d75c6c0a5ee41b287faf1158bd892dc2a4cc12e
Crowdbotics <team@crowdbotics.com>
2024-02-05 14:43:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "teknion-sample-2-45881",
+  url: "https://teknion-sample-2-45881.botics.co",
+  api: "https://teknion-sample-2-45881.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
